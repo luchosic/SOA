accef697c3603a86a5ffa55a1aa2f07f430bcb60
juanpablopumarega <42682453+juanpablopumarega@users.noreply.github.com>
2021-10-17 18:26:26 -0300

    Verificación de la conexión a internet.
    *Agrego la clase InternetConnection.
    *Verifico conexión en onCreate de la activity REgister.
    *Agrego al manifest los permisos para conectarse a internet.

--- TP2-CODIGO/app/src/main/java/com/example/tp2/RegisterActivity.java
+++ TP2-CODIGO/app/src/main/java/com/example/tp2/RegisterActivity.java
@@ -1,20 +1,37 @@
 package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.tp2.data.InternetConnection;
+import com.google.android.material.snackbar.Snackbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private InternetConnection internetConnection;
+    LinearLayout layoutWidgetNoInternetConnection;
+
     Button volverBtn;
+    Button registrateButtom;
+
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        if(InternetConnection.isOnline(this)){
+            Log.e(TAG, "Connected to Internet");
+        } else
+            Log.e(TAG, "NOT Connected to Internet");
 
         volverBtn = (Button)findViewById(R.id.volverButton);
 
@@ -25,5 +42,21 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        registrateButtom = (Button)findViewById(R.id.registrateButtom);
+
+        registrateButtom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //aca iria el código de cuando apretas REGISTRATE.
+                Log.e(TAG, "loguando");
+            }
+        });
+
+
+
+
     }
+
+
 }
