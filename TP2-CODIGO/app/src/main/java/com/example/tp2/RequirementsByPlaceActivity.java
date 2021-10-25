be47a412a32c0082bd3b0bf7324e6b7efb9b39e9
juanpablopumarega <42682453+juanpablopumarega@users.noreply.github.com>
2021-10-25 16:07:28 -0300

    Importo ambos sensores a Requirements by place

--- TP2-CODIGO/app/src/main/java/com/example/tp2/RequirementsByPlaceActivity.java
+++ TP2-CODIGO/app/src/main/java/com/example/tp2/RequirementsByPlaceActivity.java
@@ -1,7 +1,12 @@
 package com.example.tp2;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,7 +14,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RequirementsByPlaceActivity extends AppCompatActivity {
+public class RequirementsByPlaceActivity extends AppCompatActivity implements SensorEventListener {
 
     TextView requirementsTeatro;
     TextView requirementsCancha;
@@ -17,10 +22,14 @@ public class RequirementsByPlaceActivity extends AppCompatActivity {
     TextView requirementsVuelosInternacionales;
     TextView textSelectedPlace;
 
+    private SensorManager mSensorManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requirements_by_place);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -56,6 +65,86 @@ public class RequirementsByPlaceActivity extends AppCompatActivity {
             break;
         }
 
-
     }
+
+    // Metodo para iniciar el acceso a los sensores
+    protected void startSensoring() {
+       mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+       mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE), SensorManager.SENSOR_DELAY_NORMAL);
+ }
+
+    // Metodo para parar la escucha de los sensores
+    private void stopSensoring() {
+        mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+        mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE));
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String txt = "";
+        float eventoAnt = 0;
+
+        // Cada sensor puede lanzar un thread que pase por aqui
+        // Para asegurarnos ante los accesos simult�neos sincronizamos esto
+
+        synchronized (this)
+        {
+            //Log.d("sensor", event.sensor.getName());
+
+            switch(event.sensor.getType())
+            {
+                case Sensor.TYPE_ACCELEROMETER :
+                    if ((event.values[0] > 25) || (event.values[1] > 25) || (event.values[2] > 25)) {
+                        System.out.println("VIBRACION DETECTADA");
+                    }
+                    break;
+
+                case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                    txt += "Temperatura\n";
+
+                    if (event.values[0]!=eventoAnt) {
+                        eventoAnt = event.values[0];
+                        txt += event.values[0] + " C \n";
+                        System.out.println(txt);
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    protected void onStop() {
+        stopSensoring();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopSensoring();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        stopSensoring();
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        startSensoring();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startSensoring();
+    }
+
 }
